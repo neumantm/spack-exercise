c30089828411df4b01b717ac953e7cefe7e8df21
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2023-01-29 18:11:12 +0100

    Update package.py
    Co-authored-by: Ishaan Desai <ishaan.desai@ipvs.uni-stuttgart.de>

--- package.py
+++ package.py
@@ -21,5 +21,5 @@ class SpackExercise(CMakePackage):
     version("0.2.0", sha256="c421df16911fd774aa072d7f6dd1746d0890918f7d0661202771382fbc9cf3db")
     version("0.1.0", sha256="7e9513c1ff32152a707d3004bc6be91e077002963f9945cd5b16c04fc010d8ba")
 
-    depends_on("boost@1.65.1:", when="@:0.2.0")
-    depends_on("yaml-cpp@0.7.0:", when="@:0.3.0")
+    depends_on("boost@1.65.1:", when="@0.2.0:")
+    depends_on("yaml-cpp@0.7.0:", when="@0.3.0:")
